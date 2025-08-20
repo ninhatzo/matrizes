cbbae62373dc3ad9ec1b92b3ad48a216b7946140
ninhatzo <anac.estudantes2@gmail.com>
2025-08-20 20:48:57 -0300

    Corrigindo linhas e colunas atividade 1

--- atividade1/js/main.js
+++ atividade1/js/main.js
@@ -1,15 +1,15 @@
 function validaProc() {
 
     let matriz=[], resultado=[];
-    let linhas=3;
-    let colunas=3;
+    let coluna=3;
+    let linha=3;
     let num;
 
     // Alimentando a matriz
-    for(i=0; i<linhas;i++) {
-            matriz[i]=[]
-        for(c=0; c< colunas; c++) {
-            matriz[i][c] = parseInt(prompt("Digite o valor da linha " +(i+1)+ " e coluna " +(c+1)));
+    for(c=0; c<coluna;c++) {
+            matriz[c]=[]
+        for(l=0; l< linha; l++) {
+            matriz[c][l] = parseInt(prompt("Digite o valor da linha " +(l+1)+ " e coluna " +(c+1)));
         }
     }
 
@@ -17,11 +17,11 @@ function validaProc() {
     num = parseInt(prompt("Digite um número"));
 
     // Conta e imprimeiro os dados da matriz
-    for(i=0; i<linhas;i++) {
-        resultado[i]=[]
-        for(c=0; c< colunas; c++) {
-            resultado[i][c] = (matriz[i][c] * num)
-            document.write(resultado[i][c]+"&nbsp&nbsp")
+    for(c=0; c<coluna;c++) {
+        resultado[c]=[]
+        for(l=0; l< linha; l++) {
+            resultado[c][l] = (matriz[c][l] * num)
+            document.write(resultado[c][l]+"&nbsp&nbsp")
         }
             document.write("<br>");
     }
