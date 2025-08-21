81fe49ce66384341b8328fd511a51402aacb4eff
ninhatzo <anac.estudantes2@gmail.com>
2025-08-20 21:04:14 -0300

    Corrigindo linhas e colunas atividade 2

--- atividade2/js/main.js
+++ atividade2/js/main.js
@@ -1,17 +1,17 @@
 function validaProc() {
 
     let matriz=[];
-    let i, c;
-    let linhas=2;
-    let colunas=2;
+    let l, c;
+    let coluna=2;
+    let linha=2;
     let num;
     let soma=0;
 
     // Alimentando a matriz
-    for(i=0; i<linhas;i++) {
-            matriz[i]=[]
-        for(c=0; c< colunas; c++) {
-            matriz[i][c] = parseInt(prompt("Digite o valor da linha " +1+ " e coluna " +c));
+    for(c=0; c<coluna;c++) {
+            matriz[c]=[]
+        for(l=0; l< linha; l++) {
+            matriz[c][l] = parseInt(prompt("Digite o valor da linha " +l+ " e coluna " +c));
         }
     }
 
@@ -24,15 +24,15 @@ function validaProc() {
     } while ((num !== 0 && num !== 1) || isNaN(num));
 
     // Conta e imprimeiro os dados da matriz
-    for (i = 0; i < linhas; i++) {
-        if (i == num) {
-            for (c = 0; c < colunas; c++) {
-                soma += matriz[i][c];
+    for (l=0; l<linha;l++) {
+        if (l == num) {
+            for (c=0; c< coluna; c++) {
+                    soma += matriz[l][c];
             }
             document.write("Soma da linha " + num + ": " + soma + "<br>");
         }
+        document.write("<br>")
     }
-    document.write("<br>")
     return false;
 
 }
